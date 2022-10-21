7bb3273f23f8ccd54c16b85b4abd6eccd1c48a9f
Georgy Cheremnykh <g.cheremnykh@iva.ru>
2022-10-21 19:38:59 +0700

    Initial Commit

---
+++ ComradeMajor/ComradeMajorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComradeMajorApp.swift
+//  ComradeMajor
+//
+//  Created by Георгий Черемных on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ComradeMajorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
